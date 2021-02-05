cef4fee00bc37ee4c5701aaa164e9da224757068
Eduardo de Oliveira <eduardolycan@gmail.com>
2021-02-04 21:39:17 -0300

    Add Footer

---
+++ front-web/src/Footer/index.tsx
@@ -0,0 +1,21 @@
+import './styles.css';
+import { ReactComponent as YoutubeIcon} from './youtube.svg';
+import { ReactComponent as LinkedinIcon} from './linkedin.svg';
+
+function Footer(){
+    return(
+        <footer className="main-footer">
+            App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
+            <div className="footer-icons">
+                <a href="https://www.youtube.com/c/DevSuperior" target="_new">
+                    <YoutubeIcon />
+                </a>
+                <a href="https://www.linkedin.com/in/eduardo-de-oliveira-817b10189/" target="_new">
+                    <LinkedinIcon />
+                </a>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
